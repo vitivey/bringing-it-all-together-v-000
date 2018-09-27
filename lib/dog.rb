a430d6c863e3e67c07802d2a59aa3819a52671ab
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-27 16:50:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,9 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize (name:, breed:, id: nil)
-    # @name = name
-    # @breed = breed
-    # @id = id
+    @name = name
+    @breed = breed
+    @id = id
   end
 
   def self.create_table
