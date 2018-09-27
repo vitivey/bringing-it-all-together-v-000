a03ddc0a089d302c09c3302e9c554ec479ad6e2e
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-27 17:06:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,8 +46,8 @@ class Dog
         INSERT INTO dogs (name, breed)
         VALUES (?, ?)
       SQL
-      
-      DB[:conn].execute(sql, self.name, self.breed) 
+
+      DB[:conn].execute(sql, self.name, self.breed)
 
       id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").flatten.first
       self.id = id
