35f1e2a028504ac545071fd530fc342938c292c6
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-27 16:18:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ require "pry"
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize (name:, breed:)
+  def initialize (name:, breed:, id: nil)
     @name = name
     @breed = breed
   end
