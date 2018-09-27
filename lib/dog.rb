cb9059f136e678f866be3590224d74f764882dcc
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-27 16:41:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,8 +33,11 @@ class Dog
   end
 
   def save
-
-
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
   end
 
 
