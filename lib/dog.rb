d52820ecbfb954260a4f71854528aff1202575b2
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-27 16:25:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,7 +12,7 @@ class Dog
   def self.create_table
     sql = <<-SQL
       CREATE TABLE dogs (
-        id INTEGER PRIMARY KEY
+        id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
       );
